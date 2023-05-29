f86cc09a5d79ce2801bed3668f18aaba3e3834be
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-29 18:46:03 +0330

    fix: add zero then minute is lower than 10

--- utils/formatTime.ts
+++ utils/formatTime.ts
@@ -1,5 +1,7 @@
 export function formatTime(time: number) {
   const minutes = Math.floor(time / 60000);
   const seconds = (time % 60000) / 1000;
-  return `${minutes}:${+seconds < 10 ? "0" : ""}${seconds}`;
+  return `${+minutes < 10 ? "0" : ""}${minutes}:${
+    +seconds < 10 ? "0" : ""
+  }${seconds}`;
 }
