7cf8551516f8b3906b74d5037628d130befb43d6
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2024-04-02 03:08:20 +0330

    feat: create PWA application of project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    dest: "public",
+    fallbacks: {
+        //image: "/static/images/fallback.png",
+        document: "/fallback", // if you want to fallback to a custom page rather than /_offline
+        // font: '/static/font/fallback.woff2',
+        // audio: ...,
+        // video: ...,
+    },
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+    // ... other options you like
+});
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // ... other options you like
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
