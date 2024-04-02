117bbb577a5c6f30d878a36d63e52d2919a0aa82
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2024-04-02 03:42:11 +0330

    test: test pwa

--- next.config.js
+++ next.config.js
@@ -3,16 +3,31 @@ const withPWA = require("@ducanh2912/next-pwa").default({
     aggressiveFrontEndNavCaching: true,
     reloadOnOnline: true,
     swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
     dest: "public",
-    fallbacks: {
-        //image: "/static/images/fallback.png",
-        document: "/fallback", // if you want to fallback to a custom page rather than /_offline
-        // font: '/static/font/fallback.woff2',
-        // audio: ...,
-        // video: ...,
-    },
+    // fallbacks: {
+    //     image: "/static/images/fallback.png",
+    //     document: "/fallback", // if you want to fallback to a custom page rather than /_offline
+    //     font: '/static/font/fallback.woff2',
+    //     audio: ...,
+    //     video: ...,
+    // },
     workboxOptions: {
         disableDevLogs: true,
+    },
+    extendDefaultRuntimeCaching: (defaultRuntimeCaching) => {
+        // Add a new caching strategy for a specific URL pattern
+        defaultRuntimeCaching.push({
+            urlPattern: /^https?.*/,
+            handler: 'CacheFirst',
+            options: {
+                cacheName: 'api-data-cache',
+                expiration: {
+                    maxEntries: 200,
+                    maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+                },
+            },
+        });
     },
     // ... other options you like
 });
